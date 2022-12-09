f7dfb1efd3026189ff9eb97ef33bdcf91f4d37cd
Ijjaz Khan <ijazkhan19932@gmail.com>
2022-12-10 01:04:41 +0500

    function in typescript
    function in typescript

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var text1 = "Hello typescript";
+var text1 = " typescript";
+var text2 = "Javascript";
+var firstname = "ijaz";
+console.log(firstname);
+function sum(a, b) {
+    console.log("The result is: ".concat(a + b));
+    return a + b;
+}
+console.log(sum(20, 30));
--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-let text1 ="Hello typescript"
+let text1 =" typescript"
+let text2="Javascript"
+
+let firstname : string ="ijaz";
+
+console.log(firstname)
+
+function sum(a: number, b: number) {
+    console.log(`The result is: ${a + b}`);
+    return a + b;
+  }
+  
+  console.log(sum(20, 30))
+
