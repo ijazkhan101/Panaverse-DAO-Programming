73d80c086301614c34abef983504a30b605bf2ce
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-08 14:06:34 +0500

    ChakraProvider
    ChakraProvider

---
+++ Assignments/price_ui_with_nextjs_chakra_ui/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function Home() {
+  return (  <ChakraProvider>
+
+
+
+
+    <h1>Hello World </h1>
+  </ChakraProvider>
+  )
+}
