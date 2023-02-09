fd104cd3154eb0439f82d9ae845ae4d3c59c1700
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-09 20:00:26 +0500

    Panaverse website using  Chakra-UI
    Panaverse website using  Chakra-UI

---
+++ Assignments/panaverse_website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import Header from './components/Header'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Header />
+  )
+}
