cd35ec0ca774b13ffc25fef527d0be4d10af9bd1
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-09 21:14:15 +0500

    header
    header

--- Assignments/panaverse_website/src/app/components/Header.tsx
+++ Assignments/panaverse_website/src/app/components/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Box, Container, SimpleGrid, Flex ,Button} from "@chakra-ui/react";
+import logo from "../../../public/images/PanaverseLogo.png";
+import Link from "next/link";
 export default function Header() {
   return (
-    <div>
-      hello header
-    </div>
-  )
+    <Box boxShadow={'lg'}>
+      <Container maxW={1400}>
+        <SimpleGrid templateColumns="repeat(3,1fr)">
+          <Box>
+            <Image
+              src={logo}
+              alt="Panaverse Logo"
+              width={150}
+       
+            ></Image>
+          </Box>
+          <Flex gap={10} color='black' fontSize={18} fontWeight='semibold'>
+            <Link href=""> Home</Link>
+            <Link href=""> Syllabus</Link>
+            <Link href=""> Explore </Link>
+            <Link href=""> About </Link>
+            <Link href=""> Contact </Link>
+          </Flex>
+          <Box>
+            <Button float='right' colorScheme={'teal'}>Apply</Button>
+          </Box>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
 }
--- Assignments/panaverse_website/src/app/page.tsx
+++ Assignments/panaverse_website/src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-import Header from './components/Header'
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+import Header from './components/Header';
 
 export default function Home() {
   return (
