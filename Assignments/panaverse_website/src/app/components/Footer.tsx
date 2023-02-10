3fa8029e180f9a36a497bb148eaada13e2c13f47
Ijjaz Khan <ijazkhan19932@gmail.com>
2023-02-11 00:23:32 +0500

    Home
    Home

--- Assignments/panaverse_website/src/app/components/Footer.tsx
+++ Assignments/panaverse_website/src/app/components/Footer.tsx
@@ -8,21 +8,21 @@ import { Box, Container, SimpleGrid, Grid ,Flex ,Button,Heading,Text } from "@ch
 
 export default function Footer() {
     return (
-    <Box boxShadow={'lg'} background='black'>
+    <Box boxShadow={'lg'} background='white'>
       <Container maxW={1400}>
         <SimpleGrid textAlign={{lg:'start',base:'center'}} spacing='30px' py='60px' templateColumns={{base:'repeat(1, 1fr)', lg:'repeat(4, 1fr)'}}>
           <Box>
-            <Heading  pb='20px' color='white'> About US</Heading>
+            <Heading  pb='20px' color='black'> About US</Heading>
             <Image
               src={logo}
               alt="Panaverse Logo"
               width={130}
        
-            ></Image> <Text color='white'>The Future of the Web is Web 3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread</Text>
+            ></Image> <Text color='black'>The Future of the Web is Web 3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread</Text>
           </Box>
           <Box>
-          <Heading color='white'>Useful links</Heading>
-            <Grid  pt='40px' color='white'>
+          <Heading color='black'>Useful links</Heading>
+            <Grid  pt='40px' color='black'>
               <Link href=""> Home</Link>
               <Link href=""> Syllabus</Link>
               <Link href=""> Explore </Link>
@@ -31,17 +31,17 @@ export default function Footer() {
             </Grid>
           </Box>
           <Box>
-           <Heading   color='white'> Floow Us</Heading>
-            <Grid  pt='40px' color='white'>
-              <Link href=""> Facbook</Link>
-              <Link href=""> Linkedin</Link>
-              <Link href=""> Twitter </Link>
-              <Link href=""> Youtube </Link>
+           <Heading   color='black'> Floow Us</Heading>
+            <Grid  pt='40px' color='black'>
+              <Link href="https://www.facebook.com/groups/panaverse "> Facbook</Link>
+              <Link href=" https://github.com/panaverse"> Github</Link>
+              <Link href="https://twitter.com/Panaverse_edu "> Twitter </Link>
+              <Link href="https://www.youtube.com/@panaverse/streams "> Youtube </Link>
             </Grid>
           </Box>
           <Box>
-            <Heading color='white'> Contact US</Heading>
-            <Grid  pt='40px' color='white'>
+            <Heading color='black'> Contact US</Heading>
+            <Grid  pt='40px' color='black'>
               <Link href='/'>+92-308-2220203 (WhatsApp as well)</Link>
               <Link href='/'>Email   : Ijazkhan19932@gmail.com</Link>
               <Link href='/'>Address : Dir , Pakistan</Link>
--- Assignments/panaverse_website/src/app/components/Header.tsx
+++ Assignments/panaverse_website/src/app/components/Header.tsx
@@ -1,62 +1,85 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import {  Box, Button, Container, Flex, SimpleGrid, Menu, IconButton, MenuButton, MenuList, MenuItem} from "@chakra-ui/react";
-import {HamburgerIcon  } from '@chakra-ui/icons';
-import logo from "../../../public/images/PanaverseLogo.png";
-import Link from "next/link";
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  SimpleGrid,
+  Menu,
+  IconButton,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import logo from '../../../public/images/PanaverseLogo.png'
+import Link from 'next/link'
 export default function Header() {
   return (
     <Box boxShadow={'lg'}>
       <Container maxW={1400}>
         <SimpleGrid templateColumns="repeat(3,1fr)">
           <Box>
-            <Image
-              src={logo}
-              alt="Panaverse Logo"
-              width={130}
-       
-            ></Image>
+            <Image src={logo} alt="Panaverse Logo" width={130}></Image>
           </Box>
-          <Flex display={{ lg: 'flex', base: 'none' }} placeItems='center' gap={10} color='black' fontSize={18} fontWeight='semibold'>
+          <Flex
+            display={{ lg: 'flex', base: 'none' }}
+            placeItems="center"
+            gap={10}
+            color="black"
+            fontSize={18}
+            fontWeight="semibold"
+          >
             <Link href=""> Home</Link>
             <Link href=""> Syllabus</Link>
             <Link href=""> Explore </Link>
             <Link href=""> About </Link>
             <Link href=""> Contact </Link>
+            <Link href="https://github.com/panaverse"> Github</Link>
           </Flex>
           <Box display={{ lg: 'initial', base: 'none' }}>
-            <Button mt='10px' float='right' colorScheme={'teal'} size='lg'>Apply</Button>
+            <Link href="https://github.com/panaverse">
+              {' '}
+              <Button
+                mt="10px"
+                float="right"
+                ml={'4px'}
+                color="white"
+                background={'black'}
+                size="lg"
+              >
+                Github
+              </Button>
+            </Link>
+            <Link href="https://portal.piaic.org/signup">
+              {' '}
+              <Button mt="10px" float="right" colorScheme={'teal'} size="lg">
+                Apply
+              </Button>
+            </Link>
           </Box>
-          <Box pt='14px' display={{ lg: 'none', base: 'initial' }}>
-                        <Menu  >
-                            <MenuButton float='right'
-                                as={IconButton}
-                                aria-label='Options'
-                                icon={<HamburgerIcon />}
-                                variant='outline'
-                            />
-                            <MenuList>
-                                <MenuItem>
-                                    Home
-                                </MenuItem>
-                                <MenuItem>
-                                    Syllabus
-                                </MenuItem>
-                                <MenuItem>
-                                    Explore
-                                </MenuItem>
-                                <MenuItem>
-                                    About
-                                </MenuItem>
-                                <MenuItem>
-                                    Contact
-                                </MenuItem>
-                            </MenuList>
-                        </Menu>
-                    </Box>
+          <Box pt="14px" display={{ lg: 'none', base: 'initial' }}>
+            <Menu>
+              <MenuButton
+                float="right"
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem>Home</MenuItem>
+                <MenuItem>Syllabus</MenuItem>
+                <MenuItem>Explore</MenuItem>
+                <MenuItem>About</MenuItem>
+                <MenuItem>Contact</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
         </SimpleGrid>
       </Container>
     </Box>
-  );
+  )
 }
