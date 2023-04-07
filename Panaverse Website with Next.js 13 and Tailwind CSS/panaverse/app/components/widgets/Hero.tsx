68eabbd7c35bc84150b950c117a2b7ad60786ed6
ijazdev101 <ijazkhan19932@gmail.com>
2023-04-07 21:46:25 +0500

    push
    push

--- Panaverse Website with Next.js 13 and Tailwind CSS/panaverse/app/components/widgets/Hero.tsx
+++ Panaverse Website with Next.js 13 and Tailwind CSS/panaverse/app/components/widgets/Hero.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Wrapper from '../shared/Wrapper'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section>
+      <Wrapper>
+        <div>
+          {/* Left Side*/}
+            <div>
+              <h4>ji</h4>
+            </div>
+           {/* Right Side*/}
+        </div>
+      </Wrapper>
+    </section>
   )
 }
