2f7febfa974bcc2b0ce537e3500bf768b9b0f9c9
ijazdev101 <ijazkhan19932@gmail.com>
2023-04-13 23:02:06 +0500

    react js
    react js

---
+++ 0.2 Thinking in React/think-in-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Fruits from './components/Fruits'
+function App() {
+  return (
+    <div className="">
+      <header className="">
+       <h1>Think in React js </h1>
+      </header>
+      <Fruits />
+    </div>
+  );
+}
+
+export default App;
