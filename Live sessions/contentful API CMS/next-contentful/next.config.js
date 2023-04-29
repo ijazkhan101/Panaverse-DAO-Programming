105550f4dcb7debe508f6b62bb6ea6bcca93e838
ijazdev101 <ijazkhan19932@gmail.com>
2023-04-29 07:07:57 +0500

    APIs
    APIs

---
+++ Live sessions/contentful API CMS/next-contentful/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env:{
+    spaceId: 'piuof7tj3df1',
+    environment : "master",
+  },
+};
+
+module.exports = nextConfig
