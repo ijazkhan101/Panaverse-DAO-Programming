42aa6a0dcdc0b246154bacc88a02634978704028
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-17 12:34:45 +0500

    push
    push

---
+++ 03.Web Development with React/react-class/app/page.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    
+    </main>
+  )
+}
