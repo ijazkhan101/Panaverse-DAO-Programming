c5503b8517d494506af743eae6dffa5a5f73c79c
ijazdev101 <ijazkhan19932@gmail.com>
2023-05-30 20:51:50 +0500

    next js
    next js

--- 04. Next js/myapp/app/api/greeting/route.ts
+++ 04. Next js/myapp/app/api/greeting/route.ts
@@ -1,5 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async () =>{
-    return new NextResponse("This is Greeting APIs`")
+    return new NextResponse("This is Greeting APIs");
+}
+
+export const POST = async (request :  NextRequest) =>{
+     const req = await request.json();
+
+     if(req.Type){
+        return NextResponse.json({
+            status: "Cooking",
+            Cooking :  req.Type,
+            quantity :req.Quantity,
+            requestMethod : "POST",
+        });
+     }else {
+        return new NextResponse("Failed to get data")
+     }
 }
